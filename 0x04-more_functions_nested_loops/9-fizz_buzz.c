03bfdcf8468d4b96c1ce73d515d96fd6d873ee5e
denismui <denismui5454@gmail.com>
2022-10-21 04:05:37 -0700

    FizzBuzz89

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 			printf("Fizz");
 		if (n % 5 == 0)
 			printf("Buzz");
-		if (n % 3 != 0 && n % 5 != 0)
+		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz");
 	}
 	printf("\n");
