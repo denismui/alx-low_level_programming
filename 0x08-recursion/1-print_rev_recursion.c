71047cfd6416701692ec2749e156c0a2ce647c88
denismui <denismui5454@gmail.com>
2022-11-03 05:15:43 -0700

    task 11

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse using recursion
+ * @s: string s
+ */
+void _print_rev_recursion(char *s)
+{
+	s = "lpa\0";
+
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
