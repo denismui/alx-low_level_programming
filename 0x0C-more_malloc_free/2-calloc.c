bca588e4060fed4bbcad6dc499655ec8f4a792b4
denismui <denismui5454@gmail.com>
2022-11-12 09:15:30 -0800

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for nmemb byte elements
+ * @nmemb: number of elements inthe array
+ * @size: bytes for each position in the array
+ * Return: void pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+
+for (i = 0; i < nmemb * size; i++)
+p[i] = 0;
+
+return (p);
+
+}
