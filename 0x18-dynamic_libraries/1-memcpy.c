ecaf38a68cccbe1f4ef20c539e7bbdef928ca80c
denismui <denismui5454@gmail.com>
2023-01-16 02:48:15 -0800

    library[necessity]

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcopy - copies memory area
+ * @dest: dest memory area
+ * @src: src memory area
+ * @n: bytes from memory area to copy
+ * Return: memory area replaced
+ */
+char *_memcopy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] =  src[a];
+	}
+	return (a);
+}
